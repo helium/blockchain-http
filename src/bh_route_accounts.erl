a41c808f521e334f89360a22f4308b3bd677dcab
Marc Nijdam <marc_nijdam@mac.com>
2020-01-28 10:31:58 -0700

    Add single account route

--- src/bh_route_accounts.erl
+++ src/bh_route_accounts.erl
@@ -6,18 +6,22 @@
 
 -export([prepare_conn/1, handle/3]).
 %% Utilities
--export([get_account_list/2]).
+-export([get_account_list/2, get_account/1]).
 
 
 -define(S_ACCOUNT_LIST_BEFORE, "account_list_before").
 -define(S_ACCOUNT_LIST, "account_list").
+-define(S_ACCOUNT, "account").
 
 prepare_conn(Conn) ->
     {ok, _} = epgsql:parse(Conn, ?S_ACCOUNT_LIST_BEFORE,
                            "select address, dc_balance, dc_nonce, security_balance, security_nonce, balance, nonce from account_ledger  where address < $1 order by block desc, address limit $2", []),
 
     {ok, _} = epgsql:parse(Conn, ?S_ACCOUNT_LIST,
-                           "select address, dc_balance, dc_nonce, security_balance, security_nonce, balance, nonce  from account_ledger order by block desc, address limit $1", []),
+                           "select address, dc_balance, dc_nonce, security_balance, security_nonce, balance, nonce from account_ledger order by block desc, address limit $1", []),
+
+    {ok, _} = epgsql:parse(Conn, ?S_ACCOUNT,
+                           "select address, dc_balance, dc_nonce, security_balance, security_nonce, balance, nonce from account_ledger where address = $1", []),
 
     ok.
 
@@ -25,6 +29,8 @@ handle('GET', [], Req) ->
     Before = ?GET_ARG_BEFORE(Req, undefined),
     Limit = ?GET_ARG_LIMIT(Req),
     ?MK_RESPONSE(get_account_list(Before, Limit));
+handle('GET', [Account], _Req) ->
+    ?MK_RESPONSE(get_account(Account));
 
 handle(_, _, _Req) ->
     {404, [], <<"Not Found">>}.
@@ -35,6 +41,15 @@ get_account_list(undefined, Limit)  ->
 get_account_list(Before, Limit) ->
     {ok, _, Results} = ?PREPARED_QUERY(?S_ACCOUNT_LIST_BEFORE, [Before, Limit]),
     {ok, account_list_to_json(Results)}.
+
+get_account(Account) ->
+    {ok, _, [Result]} = ?PREPARED_QUERY(?S_ACCOUNT, [Account]),
+    {ok, account_to_json(Result)}.
+
+
+%%
+%% json
+%%
 
 account_list_to_json(Results) ->
     lists:map(fun account_to_json/1, Results).
