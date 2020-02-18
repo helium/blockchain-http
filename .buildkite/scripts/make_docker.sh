f04b3c6b76f2098c1f976b3ebff488e7db7ff9f4
Mark Allen <mrallen1@yahoo.com>
2020-02-18 16:43:49 -0600

    Use /mnt/tmp; try to be more direct

--- .buildkite/scripts/make_docker.sh
+++ .buildkite/scripts/make_docker.sh
@@ -5,14 +5,11 @@ set -euo pipefail
 ECS_REGISTRY_NAME="217417705465.dkr.ecr.us-west-2.amazonaws.com/dev-deploy"
 DEB_PKG="$(basename $(pwd))_$(git describe --long --always)_amd64.deb"
 DOCKER_NAME="$(basename $(pwd))_${BUILDKITE_TAG}"
-TMPDIR_NAME="/var/lib/tmp"
-
-export TMPDIR="$TMPDIR_NAME"
 
 buildkite-agent artifact download $DEB_PKG .
 
 $(aws ecr get-login --no-include-email --region us-west-2)
 
-docker build -t dev-deploy:$DOCKER_NAME -f .buildkite/scripts/Dockerfile .
+TMPDIR="/mnt/tmp" docker build -t dev-deploy:$DOCKER_NAME -f .buildkite/scripts/Dockerfile .
 docker tag dev-deploy:$DOCKER_NAME "$ECS_REGISTRY_NAME:$DOCKER_NAME"
 docker push "$ECS_REGISTRY_NAME:$DOCKER_NAME"
