3bbd10da9e4a88625562ecee9b733497e664bcf9
Marc Nijdam <marc_nijdam@mac.com>
2020-02-26 11:31:14 -0800

    Use base64url for pending txn hash

--- src/bh_route_handler.hrl
+++ src/bh_route_handler.hrl
@@ -12,5 +12,5 @@
 -define(INSERT_LAT_LON(L, N, F), bh_route_handler:lat_lon((L), (N), (F))).
 -define(INSERT_LAT_LON(L, F), bh_route_handler:lat_lon((L), (F))).
 
--define (BIN_TO_B64(B), list_to_binary(base64:encode_to_string((B)))).
+-define (BIN_TO_B64(B), base64url:encode((B))).
 -define (BIN_TO_B58(B), list_to_binary(libp2p_crypto:bin_to_b58((B)))).
