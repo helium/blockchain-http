fcf37d07afde9988633e8306fd7ec73c0ab5a2a5
Marc Nijdam <marc_nijdam@mac.com>
2020-02-27 19:31:24 -0800

    Revamp single block routes
    Includes transactions in block results for single block
    requests (height, hash)

--- src/bh_route_blocks.erl
+++ src/bh_route_blocks.erl
@@ -9,21 +9,21 @@
 %% Utilities
 -export([get_block_height/0,
          get_block_list/1,
-         get_block/1,
-         get_block_txn_list/1,
-         get_block_by_hash/1,
-         get_block_by_hash_txn_list/1]).
+         get_block_by_height/1,
+         get_block_by_hash/1]).
 
 -define(S_BLOCK_HEIGHT, "block_height").
 -define(S_BLOCK_LIST_BEFORE, "block_list_before").
 -define(S_BLOCK_LIST, "block_list").
--define(S_BLOCK, "block").
+-define(S_BLOCK_BY_HEIGHT, "block_by_height").
 -define(S_BLOCK_TXN_LIST, "block_txn_list").
 -define(S_BLOCK_BY_HASH, "block_by_hash").
 -define(S_BLOCK_BY_HASH_TXN_LIST, "block_by_hash_txn_list").
 
 -define(SELECT_BLOCK_BASE, "select b.height, b.time, b.block_hash, b.transaction_count from blocks b ").
--define(SELECT_TXN_BASE, "select t.block, t.hash, t.type, t.fields from transactions t ").
+-define(SELECT_BLOCK_TXN_BASE,
+        "select b.height, b.time, b.block_hash, b.transaction_count, t.hash, t.type, t.fields "
+        "from blocks b left join transactions t on b.height = t.block ").
 
 prepare_conn(Conn) ->
     {ok, _} = epgsql:parse(Conn, ?S_BLOCK_HEIGHT,
@@ -35,17 +35,11 @@ prepare_conn(Conn) ->
     {ok, _} = epgsql:parse(Conn, ?S_BLOCK_LIST,
                            ?SELECT_BLOCK_BASE "where b.height <= (select max(height) from blocks) order by height DESC limit $1", []),
 
-    {ok, _} = epgsql:parse(Conn, ?S_BLOCK,
-                           ?SELECT_BLOCK_BASE "where b.height = $1", []),
+    {ok, _} = epgsql:parse(Conn, ?S_BLOCK_BY_HEIGHT,
+                           ?SELECT_BLOCK_TXN_BASE "where b.height = $1", []),
 
     {ok, _} = epgsql:parse(Conn, ?S_BLOCK_BY_HASH,
-                           ?SELECT_BLOCK_BASE "where block_hash = $1", []),
-
-    {ok, _} = epgsql:parse(Conn, ?S_BLOCK_TXN_LIST,
-                          ?SELECT_TXN_BASE "where t.block = $1", []),
-
-    {ok, _} = epgsql:parse(Conn, ?S_BLOCK_BY_HASH_TXN_LIST,
-                           ?SELECT_TXN_BASE "inner join blocks b on b.height = t.block where b.block_hash = $1", []),
+                           ?SELECT_BLOCK_TXN_BASE "where b.block_hash = $1", []),
 
     ok.
 
@@ -57,21 +51,12 @@ handle('GET', [<<"height">>], _Req) ->
     ?MK_RESPONSE(get_block_height());
 handle('GET', [<<"hash">>, BlockHash], _Req) ->
     ?MK_RESPONSE(get_block_by_hash(BlockHash));
-handle('GET', [<<"hash">>, BlockHash, <<"transactions">>], _Req) ->
-    ?MK_RESPONSE(get_block_by_hash_txn_list(BlockHash));
 handle('GET', [BlockId], _Req) ->
     case catch binary_to_integer(BlockId) of
         {'EXIT', _} ->
             ?RESPONSE_400;
-        Num ->
-            ?MK_RESPONSE(get_block(Num))
-    end;
-handle('GET', [BlockId, <<"transactions">>], _Req) ->
-    case catch binary_to_integer(BlockId) of
-        {'EXIT', _} ->
-            ?RESPONSE_400;
-        Num ->
-            ?MK_RESPONSE(get_block_txn_list(Num))
+        Height ->
+            ?MK_RESPONSE(get_block_by_height(Height))
     end;
 
 handle(_Method, _Path, _Req) ->
@@ -89,50 +74,51 @@ get_block_height() ->
     {ok, _, [{Height}]} = ?PREPARED_QUERY(?S_BLOCK_HEIGHT, []),
     {ok, #{<<"height">> => Height}}.
 
-get_block(Height) ->
-    case ?PREPARED_QUERY(?S_BLOCK, [Height]) of
-        {ok, _, [Result]} ->
-            {ok, block_to_json(Result)};
-        _ ->
-            {error, not_found}
-    end.
-
-get_block_txn_list(Height) ->
-    case ?PREPARED_QUERY(?S_BLOCK_TXN_LIST, [Height]) of
-        {ok, _, Result} ->
-            {ok, bh_route_txns:txn_list_to_json(Result)};
+get_block_by_height(Height) ->
+    case ?PREPARED_QUERY(?S_BLOCK_BY_HEIGHT, [Height]) of
+        {ok, _, Results} ->
+            {ok, block_to_json(Results)};
         _ ->
             {error, not_found}
     end.
 
 get_block_by_hash(BlockHash) ->
     case ?PREPARED_QUERY(?S_BLOCK_BY_HASH, [BlockHash]) of
-        {ok, _, [Result]} ->
-            {ok, block_to_json(Result)};
+        {ok, _, Results} ->
+            {ok, block_to_json(Results)};
         _ ->
             {error, not_found}
     end.
-
-get_block_by_hash_txn_list(Hash) ->
-    case ?PREPARED_QUERY(?S_BLOCK_BY_HASH_TXN_LIST, [Hash]) of
-        {ok, _, Result} ->
-            {ok, bh_route_txns:txn_list_to_json(Result)};
-        _ ->
-            {error, not_found}
-    end.
-
 
 %%
 %% json
 %%
 
 block_list_to_json(Results) ->
-    lists:map(fun block_to_json/1, Results).
+    lists:map(fun block_base_to_json/1, Results).
 
-block_to_json({Height, Time, Hash, TxnCount}) ->
+block_base_to_json({Height, Time, Hash, TxnCount}) ->
     #{
       <<"height">> => Height,
       <<"time">> => Time,
       <<"hash">> => Hash,
-      <<"txns">> => TxnCount
+      <<"transaction_count">> => TxnCount
      }.
+
+block_to_json(Results) ->
+    %% Fold all transactions
+    Txns = lists:foldl(fun block_txn_to_json/2, [], Results),
+    %% The left join guarnateees there's always at least one
+    %% transaction in results to pull block information from.
+    {Height, Time, Hash, TxnCount, _TxnHash, _Type, _Fields} = hd(Results),
+    Block = block_base_to_json({Height, Time, Hash, TxnCount}),
+    Block#{
+           <<"transactions">> => Txns
+           }.
+
+block_txn_to_json({_Height, _Time, _Hash, _TxnCount, null, _Type, _Fields}, _Acc) ->
+    %% The left join shortcuts out to an empty transaction list if no
+    %% transactions are found.
+    [];
+block_txn_to_json({Height, _Time, _Hash, _TxnCount, Hash, Type, Fields}, Acc) ->
+    [bh_route_txns:txn_to_json({Height, Hash, Type, Fields}) | Acc].
