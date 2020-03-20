ac68700c836883bafb5b0d324c06b6c30ab655b8
Andrew Thompson <andrew@hijacked.us>
2020-03-20 16:22:37 -0700

    Attempt to switch from poolboy to dispcount

--- src/bh_db_worker.hrl
+++ src/bh_db_worker.hrl
@@ -1,5 +1,5 @@
--define(DB_RO_POOL, db_ro_pool).
--define(DB_RW_POOL, db_rw_pool).
+-define(DB_RO_POOL, persistent_term:get(ro_pool)).
+-define(DB_RW_POOL, persistent_term:get(rw_pool)).
 
 -define(SQUERY(S), ?SQUERY(?DB_RO_POOL, (S))).
 -define(EQUERY(S, A), ?EQUERY(?DB_RO_POOL, (S), (A))).
