4d5cc52c14f73f2bb405c72e612652a99f0a9cb8
Marc Nijdam <marc_nijdam@mac.com>
2020-09-08 20:00:47 -0600

    add stats/token_supply endpoint

--- test/bh_route_stats_SUITE.erl
+++ test/bh_route_stats_SUITE.erl
@@ -1,12 +1,15 @@
 -module(bh_route_stats_SUITE).
+
 -compile([nowarn_export_all, export_all]).
 
 -include("ct_utils.hrl").
 -include("../src/bh_route_handler.hrl").
 
-all() -> [
-          stats_test
-         ].
+all() ->
+    [
+        stats_test,
+        token_supply_test
+    ].
 
 init_per_suite(Config) ->
     ?init_bh(Config).
@@ -16,10 +19,19 @@ end_per_suite(Config) ->
 
 stats_test(_Config) ->
     {ok, {_, _, Json}} = ?json_request("/v1/stats"),
-    #{ <<"data">> := #{
-                       <<"block_times">> := _ ,
-                       <<"election_times">> := _,
-                       <<"token_supply">> := _
-                      }
-     } = Json,
+    #{
+        <<"data">> := #{
+            <<"block_times">> := _,
+            <<"election_times">> := _,
+            <<"token_supply">> := _
+        }
+    } = Json,
+    ok.
+
+token_supply_test(_Config) ->
+    {ok, {_, _, Json}} = ?json_request("/v1/stats/token_supply"),
+    #{<<"data">> := #{<<"token_supply">> := _}} = Json,
+
+    {ok, {_, _, Value}} = ?json_request("/v1/stats/token_supply?format=raw"),
+    ?assert(Value >= 0),
     ok.
