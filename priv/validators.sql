ae4e542452ba1305acd66310da9ed10bf8c4db3a
Marc Nijdam <marc_nijdam@mac.com>
2021-02-23 10:32:38 -0600

    Add v1/validators routes

---
+++ priv/validators.sql
@@ -0,0 +1,28 @@
+-- :validator_list_base
+select
+    (select max(height) from blocks) as height,
+    l.address,
+    l.owner,
+    l.stake,
+    l.last_heartbeat,
+    l.version_heartbeat,
+    l.nonce,
+    l.first_block
+    :extend
+from validator_inventory l
+:scope
+:order
+:limit
+
+-- :validator_list_order
+order by l.first_block desc, l.address
+
+-- :validator_list_before_scope
+where (l.address > $1 and l.first_block = $2) or (l.first_block < $2)
+
+-- :validator_speculative_extend
+, (select greatest(l.nonce, coalesce(max(p.nonce), l.nonce))
+    from pending_transactions p
+    where p.address = l.address and nonce_type='validator' and status != 'failed'
+) as speculative_nonce
+
---
+++ src/bh_route_validators.erl
@@ -0,0 +1,122 @@
+-module(bh_route_validators).
+
+-behavior(bh_route_handler).
+-behavior(bh_db_worker).
+
+-include("bh_route_handler.hrl").
+
+-export([prepare_conn/1, handle/3]).
+%% Utilities
+-export([get_validator_list/1, get_validator/1]).
+
+-define(S_VALIDATOR_LIST_BEFORE, "validator_list_before").
+-define(S_VALIDATOR_LIST, "validator_list").
+-define(S_VALIDATOR, "validator").
+-define(VALIDATOR_LIST_LIMIT, 100).
+
+prepare_conn(Conn) ->
+    ValidatorListLimit = "limit " ++ integer_to_list(?VALIDATOR_LIST_LIMIT),
+    Loads = [
+        {?S_VALIDATOR_LIST_BEFORE,
+            {validator_list_base, [
+                {extend, ""},
+                {scope, validator_list_before_scope},
+                {order, validator_list_order},
+                {limit, ValidatorListLimit}
+            ]}},
+        {?S_VALIDATOR_LIST,
+            {validator_list_base, [
+                {extend, ""},
+                {scope, ""},
+                {order, validator_list_order},
+                {limit, ValidatorListLimit}
+            ]}},
+        {?S_VALIDATOR,
+            {validator_list_base, [
+                {extend, validator_speculative_extend},
+                {scope, "where l.address = $1"},
+                {order, ""},
+                {limit, ""}
+            ]}}
+    ],
+    bh_db_worker:load_from_eql(Conn, "validators.sql", Loads).
+
+handle('GET', [], Req) ->
+    Args = ?GET_ARGS([cursor], Req),
+    ?MK_RESPONSE(get_validator_list(Args), block_time);
+handle('GET', [Address], _Req) ->
+    ?MK_RESPONSE(get_validator(Address), never);
+handle(_, _, _Req) ->
+    ?RESPONSE_404.
+
+get_validator_list([{cursor, undefined}]) ->
+    Result = ?PREPARED_QUERY(?S_VALIDATOR_LIST, []),
+    mk_validator_list_from_result(Result);
+get_validator_list([{cursor, Cursor}]) ->
+    case ?CURSOR_DECODE(Cursor) of
+        {ok, #{
+            <<"before_address">> := BeforeAddress,
+            <<"before_block">> := BeforeBlock,
+            <<"height">> := _Height
+        }} ->
+            Result =
+                ?PREPARED_QUERY(?S_VALIDATOR_LIST_BEFORE, [BeforeAddress, BeforeBlock]),
+            mk_validator_list_from_result(Result);
+        _ ->
+            {error, badarg}
+    end.
+
+get_validator(Address) ->
+    case ?PREPARED_QUERY(?S_VALIDATOR, [Address]) of
+        {ok, _, [Result]} ->
+            {ok, validator_to_json(Result)};
+        _ ->
+            {error, not_found}
+    end.
+
+mk_validator_list_from_result({ok, _, Results}) ->
+    {ok, validator_list_to_json(Results), mk_cursor(Results)}.
+
+mk_cursor(Results) when is_list(Results) ->
+    case length(Results) < ?VALIDATOR_LIST_LIMIT of
+        true ->
+            undefined;
+        false ->
+            {Height, Address, _Owner, _Stake, _LastHeartbeat, _VersionHeartBeat, _Nonce,
+                FirstBlock} = lists:last(Results),
+            #{
+                before_address => Address,
+                before_block => FirstBlock,
+                height => Height
+            }
+    end.
+
+%%
+%% json
+%%
+
+validator_list_to_json(Results) ->
+    lists:map(fun validator_to_json/1, Results).
+
+validator_to_json(
+    {Height, Address, Owner, Stake, LastHeartbeat, VersionHeartbeat, Nonce, _FirstBlock}
+) ->
+    #{
+        <<"address">> => Address,
+        <<"owner">> => Owner,
+        <<"nonce">> => Nonce,
+        <<"stake">> => Stake,
+        <<"last_heartbeat">> => LastHeartbeat,
+        <<"version_heartbeat">> => VersionHeartbeat,
+        <<"block">> => Height
+    };
+validator_to_json(
+    {Height, Address, Owner, Stake, LastHeartbeat, VersionHeartbeat, Nonce, FirstBlock,
+        SpecNonce}
+) ->
+    Base = validator_to_json(
+        {Height, Address, Owner, Stake, LastHeartbeat, VersionHeartbeat, Nonce, FirstBlock}
+    ),
+    Base#{
+        <<"speculative_nonce">> => SpecNonce
+    }.
