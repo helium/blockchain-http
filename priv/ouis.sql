5b21c20de4bc322df21a71c652c9c61dd47da3b5
Marc Nijdam <marc_nijdam@mac.com>
2021-03-08 07:03:08 -0600

    Add oui routes

---
+++ priv/ouis.sql
@@ -0,0 +1,29 @@
+-- :oui_list_base
+select
+    (select max(height) from blocks) as height,
+    l.oui,
+    l.owner,
+    l.nonce,
+    l.addresses,
+    l.subnets,
+    l.first_block
+from oui_inventory l
+:scope
+:order
+:limit
+
+-- :oui_list_order
+order by l.first_block desc, l.oui
+
+-- :oui_list_before_scope
+where (l.oui > $1 and l.first_block = $2) or (l.first_block < $2)
+
+-- :owner_oui_list_scope
+where l.owner = $1
+
+-- :owner_oui_list_before_scope
+where l.owner = $1
+    and ((l.oui > $2 and l.first_block = $3) or (l.first_block < $3))
+
+-- :oui_active
+select * from stats_inventory where name = 'ouis'
--- src/bh_route_ouis.erl
+++ src/bh_route_ouis.erl
@@ -9,35 +9,128 @@
 %% Utilities
 -export([get_current_oui/0]).
 
+-define(S_OUI_LIST_BEFORE, "oui_list_before").
+-define(S_OUI_LIST, "oui_list").
+-define(S_OWNER_OUI_LIST_BEFORE, "owner_oui_list_before").
+-define(S_OWNER_OUI_LIST, "owner_oui_list").
+-define(S_OUI, "oui").
+-define(S_LAST_OUI, "last_oui").
+-define(S_ACTIVE_OUIS, "active_ouis").
+
+-define(OUI_LIST_LIMIT, 100).
+
 -define(S_OUI_CURRENT, "oui_current").
 
 prepare_conn(Conn) ->
-    {ok, S1} = epgsql:parse(
-        Conn,
-        ?S_OUI_CURRENT,
-        [
-            ?SELECT_TXN_BASE,
-            "from transactions t where type = 'oui_v1' ",
-            "order by block desc limit 1"
-        ],
-        []
-    ),
-    #{
-        ?S_OUI_CURRENT => S1
-    }.
+    OuiListLimit = "limit " ++ integer_to_list(?OUI_LIST_LIMIT),
+    Loads = [
+        {?S_OUI_LIST_BEFORE,
+            {oui_list_base, [
+                {scope, oui_list_before_scope},
+                {order, oui_list_order},
+                {limit, OuiListLimit}
+            ]}},
+        {?S_OUI_LIST,
+            {oui_list_base, [
+                {scope, ""},
+                {order, oui_list_order},
+                {limit, OuiListLimit}
+            ]}},
+        {?S_OWNER_OUI_LIST_BEFORE,
+            {oui_list_base, [
+                {scope, owner_oui_list_before_scope},
+                {order, oui_list_order},
+                {limit, OuiListLimit}
+            ]}},
+        {?S_OWNER_OUI_LIST,
+            {oui_list_base, [
+                {scope, owner_oui_list_scope},
+                {order, oui_list_order},
+                {limit, OuiListLimit}
+            ]}},
+        {?S_LAST_OUI,
+            {oui_list_base, [
+                {scope, "where oui = (select max(oui) from ouis)"},
+                {order, ""},
+                {limit, ""}
+            ]}},
+        {?S_OUI,
+            {oui_list_base, [
+                {scope, "where oui = $1"},
+                {order, ""},
+                {limit, ""}
+            ]}},
+        {?S_ACTIVE_OUIS, {oui_active, []}}
+    ],
+
+    bh_db_worker:load_from_eql(Conn, "ouis.sql", Loads).
 
 handle('GET', [], Req) ->
-    Args = add_filter_types(?GET_ARGS([cursor], Req)),
-    Result = bh_route_txns:get_txn_list(Args),
-    CacheTime = bh_route_txns:get_txn_list_cache_time(Result),
-    ?MK_RESPONSE(Result, CacheTime);
+    Args = ?GET_ARGS([cursor], Req),
+    ?MK_RESPONSE(get_oui_list([{owner, undefined} | Args]), block_time);
+handle('GET', [<<"stats">>], _Req) ->
+    ?MK_RESPONSE(get_stats(), block_time);
 handle('GET', [<<"last">>], _Req) ->
-    ?MK_RESPONSE(get_current_oui(), block_time);
+    ?MK_RESPONSE(get_last_oui(), block_time);
+handle('GET', [OuiBin], _Req) ->
+    try binary_to_integer(OuiBin) of
+        Oui -> ?MK_RESPONSE(get_oui(Oui), infinity)
+    catch
+        _:_ ->
+            ?RESPONSE_400
+    end;
 handle(_, _, _Req) ->
     ?RESPONSE_404.
 
-add_filter_types(Args) ->
-    Args ++ [{filter_types, <<"oui_v1">>}].
+get_oui_list([{owner, undefined}, {cursor, undefined}]) ->
+    Result = ?PREPARED_QUERY(?S_OUI_LIST, []),
+    mk_oui_list_from_result(Result);
+get_oui_list([{owner, Owner}, {cursor, undefined}]) ->
+    Result = ?PREPARED_QUERY(?S_OWNER_OUI_LIST, [Owner]),
+    mk_oui_list_from_result(Result);
+get_oui_list([{owner, Owner}, {cursor, Cursor}]) ->
+    case ?CURSOR_DECODE(Cursor) of
+        {ok, #{
+            <<"before_address">> := BeforeAddress,
+            <<"before_block">> := BeforeBlock,
+            <<"height">> := _Height
+        }} ->
+            case Owner of
+                undefined ->
+                    Result =
+                        ?PREPARED_QUERY(?S_OUI_LIST_BEFORE, [
+                            BeforeAddress,
+                            BeforeBlock
+                        ]),
+                    mk_oui_list_from_result(Result);
+                Owner ->
+                    Result =
+                        ?PREPARED_QUERY(?S_OWNER_OUI_LIST_BEFORE, [
+                            Owner,
+                            BeforeAddress,
+                            BeforeBlock
+                        ]),
+                    mk_oui_list_from_result(Result)
+            end;
+        _ ->
+            {error, badarg}
+    end.
+
+get_oui(Oui) ->
+    case ?PREPARED_QUERY(?S_OUI, [Oui]) of
+        {ok, _, [Result]} -> {ok, oui_to_json(Result)};
+        _ -> {error, not_found}
+    end.
+
+get_last_oui() ->
+    case ?PREPARED_QUERY(?S_LAST_OUI, []) of
+        {ok, _, [Result]} -> {ok, oui_to_json(Result)};
+        _ -> {error, not_found}
+    end.
+
+get_stats() ->
+    CountStats = ?PREPARED_QUERY(?S_ACTIVE_OUIS, []),
+    mk_stats_from_results(CountStats).
 
 get_current_oui() ->
     {ok, _, Results} = ?PREPARED_QUERY(?S_OUI_CURRENT, []),
@@ -45,3 +138,41 @@ get_current_oui() ->
         [] -> {error, not_found};
         [Result] -> {ok, bh_route_txns:txn_to_json(Result)}
     end.
+
+mk_oui_list_from_result({ok, _, Results}) ->
+    {ok, oui_list_to_json(Results), mk_cursor(Results)}.
+
+mk_stats_from_results({ok, _, [{Count}]}) ->
+    {ok, #{count => Count}}.
+
+mk_cursor(Results) when is_list(Results) ->
+    case length(Results) < ?OUI_LIST_LIMIT of
+        true ->
+            undefined;
+        false ->
+            {Height, Oui, _Owner, _Nonce, _Addresses, _Subnets, FirstBlock} = lists:last(Results),
+            #{
+                before_oui => Oui,
+                before_block => FirstBlock,
+                height => Height
+            }
+    end.
+
+%%
+%% json
+%%
+
+oui_list_to_json(Results) ->
+    lists:map(fun oui_to_json/1, Results).
+
+oui_to_json(
+    {Height, Oui, Owner, Nonce, Addresses, Subnets, _FirstBlock}
+) ->
+    #{
+        block => Height,
+        oui => Oui,
+        owner => Owner,
+        nonce => Nonce,
+        addresses => Addresses,
+        subnets => Subnets
+    }.
