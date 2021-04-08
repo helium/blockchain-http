adeee5084f7bc63288802de206032780fdd76026
Marc Nijdam <marc@helium.com>
2021-04-07 21:27:51 -0600

    Add vars_v1 ingest (#226)

--- src/bh_route_pending_txns.erl
+++ src/bh_route_pending_txns.erl
@@ -114,6 +114,7 @@ handle(_, _, _Req) ->
 -type supported_txn() ::
     #blockchain_txn_oui_v1_pb{}
     | #blockchain_txn_routing_v1_pb{}
+    | #blockchain_txn_vars_v1_pb{}
     | #blockchain_txn_add_gateway_v1_pb{}
     | #blockchain_txn_assert_location_v1_pb{}
     | #blockchain_txn_assert_location_v2_pb{}
@@ -137,6 +138,9 @@ insert_pending_txn(#blockchain_txn_routing_v1_pb{nonce = Nonce} = Txn, Bin) ->
     %% address for it (an oui is not a public key which a lot of code relies on, we don't
     %% track it right now. We can't lean on the owner address since an owner can
     %% have multipe ouis
+    insert_pending_txn(Txn, undefined, Nonce, <<"none">>, Bin);
+insert_pending_txn(#blockchain_txn_vars_v1_pb{nonce = Nonce} = Txn, Bin) ->
+    %% A vars transaction doesn't have a clear actor at all so we don't track it
     insert_pending_txn(Txn, undefined, Nonce, <<"none">>, Bin);
 insert_pending_txn(#blockchain_txn_add_gateway_v1_pb{gateway = GatewayAddress} = Txn, Bin) ->
     %% Adding a gateway uses the gateway nonce, even though it's
@@ -313,6 +317,7 @@ txn_unwrap(#blockchain_txn_pb{txn = {_, Txn}}) ->
     blockchain_txn_routing_v1_pb,
     {signature = <<>>}
 );
+?TXN_HASH(blockchain_txn_vars_v1_pb, {});
 ?TXN_HASH(
     blockchain_txn_add_gateway_v1_pb,
     {owner_signature = <<>>, gateway_signature = <<>>}
@@ -339,6 +344,7 @@ txn_unwrap(#blockchain_txn_pb{txn = {_, Txn}}) ->
 
 ?TXN_TYPE(blockchain_txn_oui_v1_pb, <<"oui_v1">>);
 ?TXN_TYPE(blockchain_txn_routing_v1_pb, <<"routing_v1">>);
+?TXN_TYPE(blockchain_txn_vars_v1_pb, <<"vars_v1">>);
 ?TXN_TYPE(blockchain_txn_add_gateway_v1_pb, <<"add_gateway_v1">>);
 ?TXN_TYPE(blockchain_txn_assert_location_v1_pb, <<"assert_location_v1">>);
 ?TXN_TYPE(blockchain_txn_assert_location_v2_pb, <<"assert_location_v2">>);
