3532e7216903a88332e367df9b8ddfde25f8d1fc
Marc Nijdam <marc_nijdam@mac.com>
2021-11-01 19:33:10 -0700

    Add keys query parameter to vars

--- priv/vars.sql
+++ priv/vars.sql
@@ -3,3 +3,6 @@ SELECT v.name, v.type, v.value from vars_inventory v order by name
 
 -- :var_get
 SELECT v.name, v.type, v.value from vars_inventory v where v.name = $1
+
+-- :var_list_named
+SELECT v.name, v.type, v.value from vars_inventory v where v.name = ANY($1)
