545099b5117cca047e2a07166accd594f546f8cd
Andrew Thompson <andrew@hijacked.us>
2021-11-24 14:18:47 -0800

    Add transaction_type enum

--- src/bh_route_pending_txns.erl
+++ src/bh_route_pending_txns.erl
@@ -44,7 +44,23 @@
     integer_to_list(?PENDING_TXN_LIST_LIMIT)
 ]).
 
-prepare_conn(_Conn) ->
+prepare_conn(Conn) ->
+	epgsql:update_type_cache(Conn, [{bh_transaction_type, [coinbase_v1,security_coinbase_v1,
+                                                         oui_v1,gen_gateway_v1,routing_v1,
+                                                         payment_v1,security_exchange_v1,
+                                                         consensus_group_v1,add_gateway_v1,
+                                                         assert_location_v1,create_htlc_v1,
+                                                         redeem_htlc_v1,poc_request_v1,
+                                                         poc_receipts_v1,vars_v1,rewards_v1,
+                                                         token_burn_v1,dc_coinbase_v1,
+                                                         token_burn_exchange_rate_v1,payment_v2,
+                                                         state_channel_open_v1,state_channel_close_v1,
+                                                         price_oracle_v1,transfer_hotspot_v1,
+                                                         rewards_v2,assert_location_v2,
+                                                         gen_validator_v1,stake_validator_v1,
+                                                         unstake_validator_v1,validator_heartbeat_v1,
+                                                         transfer_validator_stake_v1,gen_price_oracle_v1,
+                                                         consensus_group_failure_v1,transfer_hotspot_v2]}]),
     S1 = {
             ?SELECT_ACTOR_PENDING_TXN_LIST_BASE(""),
             [text]
